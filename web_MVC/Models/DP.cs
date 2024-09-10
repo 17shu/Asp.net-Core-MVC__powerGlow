f7342df38b8050732307b4568854e90abec1c7ab
unknown <AceEnergy@AEGEmilyLinPC.acepillar.local>
2024-09-10 09:27:01 +0800

    create control page

---
+++ web_MVC/Models/DP.cs
@@ -0,0 +1,11 @@
+﻿namespace web_MVC.Models
+{
+    public class DP
+    {
+
+        public string Name { get; set; }
+        public double Value { get; set; }
+
+        public DateTime Datetime { get; set; }
+    }
+}
